dab537d2fb939bacd78884d2fbf8cd9cf026de3e
seniorQAautomationEngineer <oleksii.lavrenin@gmail.com>
2022-07-17 11:18:56 -0400

    Added WebDriver wait

--- src/test/java/BaseUI.java
+++ src/test/java/BaseUI.java
@@ -1,18 +1,23 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
 
 public class BaseUI {
 
     WebDriver driver;
     String url = "https://romanceabroad.com/";
+    WebDriverWait wait;
 
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
         driver.get(url);
         driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
 
