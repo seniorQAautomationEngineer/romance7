0b4e1703580283f9e71625ec13cb0191ccbef4a9
seniorQAautomationEngineer <oleksii.lavrenin@gmail.com>
2022-07-24 11:17:24 -0400

    Created registration and iframe tests

--- src/test/java/MainTests.java
+++ src/test/java/MainTests.java
@@ -1,5 +1,10 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 
 public class MainTests extends BaseUI{
@@ -11,28 +16,51 @@ public class MainTests extends BaseUI{
     By textFieldEmail = By.cssSelector("input[id='email']");
     By textFieldPassword = By.cssSelector("input[id='password']");
     By nextButton = By.xpath("//button[@data-action='next-page'][text()='Next']");
+    By nicknameTextField = By.xpath("//input[@id='nickname']");
+    By dayDropDown = By.xpath("//div[@id='daySelect']");
+    By selectedDay = By.xpath("//ul[@class ='dropdown-menu show']//li[@data-value='22']");
+    By regionNameAutocomplete = By.xpath("//input[@name='region_name']");
+    By regionSelect = By.xpath("//div[contains(@id, 'region_select')]");
+    By selectedCapitalOfUkraine = By.xpath("//li[text()='Kiev, Ukraine']");
+    By confirmationCheckbox = By.xpath("//input[@id='confirmation']");
+    By youtubeFrameLocator = By.xpath("//iframe[@src='https://www.youtube.com/embed/RRECuJzm3IY?start=85']");
+    By playYoutubeButton = By.xpath("//button[@aria-label='Play']");
 
     String email = "1111111111111@gmail.com";
     String password = "1234567@";
 
+    String nickname = "al838383";
+    String city = "Kiev";
+
+
 
 
     @Test
-    public void mainPageTests7() {
+    public void completeRegistration() {
         driver.findElement(registrationButton).click();
         driver.findElement(textFieldEmail).sendKeys(email);
         driver.findElement(textFieldPassword).sendKeys(password);
         driver.findElement(nextButton).click();
+        driver.findElement(nicknameTextField).sendKeys(nickname);
+        driver.findElement(dayDropDown).click();
+        driver.findElement(selectedDay).click();
+        driver.findElement(confirmationCheckbox).click();
+        driver.findElement(regionNameAutocomplete).clear();
+        driver.findElement(regionNameAutocomplete).sendKeys(city);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(regionSelect)));
+        driver.findElement(selectedCapitalOfUkraine).click();
+
 
     }
 
-    @Test
-    public void mainPageTests8() {
-        driver.findElement(registrationButton).click();
-        driver.findElement(textFieldEmail).sendKeys(email);
-        driver.findElement(textFieldPassword).sendKeys(password);
-        driver.findElement(nextButton).click();
 
+    @Test
+    public void testIframe(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        WebElement youtubeIframe = driver.findElement(youtubeFrameLocator);
+        driver.switchTo().frame(youtubeIframe);
+        driver.findElement(playYoutubeButton).click();
     }
 
 
